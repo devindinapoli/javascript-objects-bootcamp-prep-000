5c7b3e4f68a7e9fef037f0e0d7318eb95218cd94
Devin DiNapoli <devindinapoli@gmail.com>
2018-01-31 23:17:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, aristName){
- delete artistName;
+ delete playlist[artistName];
   return playlist;
 }
